98ae9e587cbd5ac10400ee68c37041444512d5b5
Ayush Bholan <ayush.456bholan@gmail.com>
2026-04-17 13:48:36 +0545

    feat:add navbar components

---
+++ frontend/app/ components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,12 @@
+
+
+const AddPropertyButton = () => {
+  return (
+    <div className="cursor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200">
+
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default AddPropertyButton
---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      Django bnb
+      <h2 className="text-stayfinder">django</h2>
+    </main>
+  );
+} 

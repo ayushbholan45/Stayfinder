415dc404c0720ec91e0f861af8d432b2eda42ab9
Ayush Bholan <ayush.456bholan@gmail.com>
2026-04-22 11:02:05 +0545

    fix: add static media URL configuration for property images

--- backend/stayfinder_backend/settings.py
+++ backend/stayfinder_backend/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_AUTH = {
     "USE_JWT": True,
     "JWT_AUTH_HTTPONLY": False
@@ -169,5 +171,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_URL = "media/"
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

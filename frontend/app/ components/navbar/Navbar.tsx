0a9c806d3f620eebac5e13135403cc3ea2b833fb
Ayush Bholan <ayush.456bholan@gmail.com>
2026-04-22 11:00:23 +0545

    fix the svg file

--- frontend/app/ components/navbar/Navbar.tsx
+++ frontend/app/ components/navbar/Navbar.tsx
@@ -6,25 +6,27 @@ import AddPropertyButton from "./AddPropertyButton"
 
 const Navbar = () => {
   return (
-    <nav className="w-full fixed top-0 left-0 py-6 border-b border-gray-200  bg-white z-10">
-        <div className="max-w-400 mx-auto px-6">
-            <div className="flex justify-between items-center">
-                <Link href="/">
-                    <Image src="/logo.svg" alt="stayfinder logo" width={180} height={38}
-                    />
-                </Link>
-
-                <div className="flex space-x-6">
-                    <SearchFilters/>
-                </div>
-
-                <div className="flex items-center space-x-6">
-                    <AddPropertyButton/>
-                    <UserNav/>
-                </div>
-            </div>
-
+    <nav className="w-full fixed top-0 left-0 py-6 border-b border-gray-200 bg-white z-10">
+      <div className="max-w-400 mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="stayfinder logo"
+              width={180}
+              height={38}
+              priority
+            />
+          </Link>
+          <div className="flex space-x-6">
+            <SearchFilters/>
+          </div>
+          <div className="flex items-center space-x-6">
+            <AddPropertyButton/>
+            <UserNav/>
+          </div>
         </div>
+      </div>
     </nav>
   )
 }

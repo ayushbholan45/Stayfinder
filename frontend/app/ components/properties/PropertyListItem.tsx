50105173a00fb068f4e05de0735220421ef86724
Ayush Bholan <ayush.456bholan@gmail.com>
2026-04-23 18:42:00 +0545

    feat:detail page for properties

--- frontend/app/ components/properties/PropertyListItem.tsx
+++ frontend/app/ components/properties/PropertyListItem.tsx
@@ -1,19 +1,28 @@
 import Image from "next/image"
 import { PropertyType } from "./PropertyList"
+import { useRouter } from "next/navigation"
 
 interface PropertyProps {
     property: PropertyType
 }
 
 const PropertyListItem: React.FC< PropertyProps>= ({ property }) => {
+    const router = useRouter();
+
+
+
+
   return (
-    <div className="cursor-pointer">
+    <div
+        className="cursor-pointer"
+        onClick={() => router.push(`/properties/${property.id}`) } 
+    >
         <div className="relative overflow-hidden aspect-square rounded-xl">
             <Image fill src = {property.image_url} sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
             className="hover:scale-110 object-cover transition h-full w-full"
             alt= "Beach house"/>
         </div>
-
+ 
         <div className="mt-2">
             <p className="text-lg font-bold">{property.title}</p>
         </div>
--- frontend/app/properties/[id]/page.tsx
+++ frontend/app/properties/[id]/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image"
 import ReservationSidebar from "@/app/ components/properties/ReservationSidebar"
+import apiService from "@/app/services/apiService"
 
-const PropertyDetailPage = () => {
+const PropertyDetailPage = async ({ params}: { params : Promise < {id: string } >}) => {
+  const { id } = await params;
+  const property = await apiService.get(`/api/properties/${id}`)
   return (
     <main className="max-w-6xl mx-auto px-6 pb-6">
         <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -10,33 +13,38 @@ const PropertyDetailPage = () => {
 
         <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
           <div className="py-6 pr-6 col-span-3">
-            <h1 className="mb-4 text-4xl">Property name</h1>
+            <h1 className="mb-4 text-4xl">{property.title}</h1>
             <span className="mb-6 block text-lg text-gray-600">
-              4 guests - 2 bedrooms - 1 bathroom
+              {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
             </span>
 
             <hr className="opacity-20" />
 
             <div className="py-6 flex items-center space-x-4">
-              <Image 
-                src="/profile_pic1.jpg" 
-                width={50} 
-                height={50} 
-                className="rounded-full w-12.5 h-12.5 object-cover" 
-                alt="The user name"
-              />
+              {property.landlord.avatar_url && (
+                  <Image 
+                    src={property.landlord.avatar_url}
+                    width={50} 
+                    height={50} 
+                    className="rounded-full w-12.5 h-12.5 object-cover" 
+                    alt="The user name"
+                  />
+              )}
+              
 
-              <p><strong>John Doe </strong>is your host</p>
+              <p><strong>{property.landlord.name}</strong> is your host</p>
             </div>
 
             <hr className="opacity-20" />
 
             <p className="mt-6 text-lg">
-              dsdsadsdsads
+              {property.description}
             </p>
           </div>
 
-          <ReservationSidebar/>
+          <ReservationSidebar
+              property={property}
+              />
         </div>
     </main>
   )

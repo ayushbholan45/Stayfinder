283068cf03064ff9933b44329c2ffec53f05d907
Ayush Bholan <ayush.456bholan@gmail.com>
2026-04-24 20:59:03 +0545

    feat:add search up filters dynamically

--- frontend/app/ components/properties/PropertyList.tsx
+++ frontend/app/ components/properties/PropertyList.tsx
@@ -1,16 +1,18 @@
 'use client';
 
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import PropertyListItem from "./PropertyListItem";
 import apiService from '@/app/services/apiService';
-import {useEffect, useState} from 'react'
-
-import PropertyListItem from "./PropertyListItem"
+import useSearchModal from '@/app/hooks/useSearchModal';
 
 export type PropertyType = {
-  id: string;
-  title: string;
-  price_per_night: number;
-  image_url : string;
-  is_favorite: boolean;
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+    is_favorite: boolean;
 }
 
 interface PropertyListProps {
@@ -22,9 +24,21 @@ const PropertyList: React.FC<PropertyListProps> = ({
     landlord_id,
     favorites
 }) => {
+    const params = useSearchParams();
+    const searchModal = useSearchModal();
+    const country = searchModal.query.country;
+    const numGuests = searchModal.query.guests;
+    const numBathrooms = searchModal.query.bathrooms;
+    const numBedrooms = searchModal.query.bedrooms;
+    const checkinDate = searchModal.query.checkIn;
+    const checkoutDate = searchModal.query.checkOut;
+    const category = searchModal.query.category;
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
-     const markFavorite = (id: string, is_favorite: boolean) => {
+    console.log('searchQUery:', searchModal.query);
+    console.log('numBedrooms', numBedrooms)
+
+    const markFavorite = (id: string, is_favorite: boolean) => {
         const tmpProperties = properties.map((property: PropertyType) => {
             if (property.id == id) {
                 property.is_favorite = is_favorite
@@ -42,48 +56,83 @@ const PropertyList: React.FC<PropertyListProps> = ({
         setProperties(tmpProperties);
     }
 
-    const getProperties = async() =>{
-      let url = '/api/properties/';
+    const getProperties = async () => {
+        let url = '/api/properties/';
 
-      if (landlord_id) {
+        if (landlord_id) {
             url += `?landlord_id=${landlord_id}`
-        }else if (favorites) {
+        } else if (favorites) {
             url += '?is_favorites=true'
+        } else {
+            let urlQuery = '';
+
+            if (country) {
+                urlQuery += '&country=' + country
+            }
+
+            if (numGuests) {
+                urlQuery += '&numGuests=' + numGuests
+            }
+
+            if (numBedrooms) {
+                urlQuery += '&numBedrooms=' + numBedrooms
+            }
+
+            if (numBathrooms) {
+                urlQuery += '&numBathrooms=' + numBathrooms
+            }
+
+            if (category) {
+                urlQuery += '&category=' + category
+            }
+
+            if (checkinDate) {
+                urlQuery += '&checkin=' + format(checkinDate, 'yyyy-MM-dd')
+            }
+
+            if (checkoutDate) {
+                urlQuery += '&checkout=' + format(checkoutDate, 'yyyy-MM-dd')
+            }
+
+            if (urlQuery.length) {
+                console.log('Query:', urlQuery);
+
+                urlQuery = '?' + urlQuery.substring(1);
+
+                url += urlQuery;
+            }
         }
-        
-      const tmpProperties = await apiService.get(url)
 
-      setProperties(tmpProperties.data.map((property: PropertyType) =>  {
-        if(tmpProperties.favorites.includes(property.id)){
-          property.is_favorite = true
-        } else{
-            property.is_favorite = false
-        }
+        const tmpProperties = await apiService.get(url)
 
-        return property;
-      }));
+        setProperties(tmpProperties.data.map((property: PropertyType) => {
+            if (tmpProperties.favorites.includes(property.id)) {
+                property.is_favorite = true
+            } else {
+                property.is_favorite = false
+            }
 
-    }
+            return property
+        }));
+    };
 
-  useEffect(() => {
-    
-    getProperties()
-  }, [])
+    useEffect(() => {
+        getProperties();
+    }, [category, searchModal.query, params]);
 
-  return (
-    <>
-      {properties.map((property) => {
-        return(
-            <PropertyListItem
-              key={property.id}
-              property={property}
-              markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
-              />
-        )
-      })}
-      
-    </>
-  )
+    return (
+        <>
+            {properties.map((property) => {
+                return (
+                    <PropertyListItem 
+                        key={property.id}
+                        property={property}
+                        markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
+                    />
+                )
+            })}
+        </>
+    )
 }
 
-export default PropertyList
+export default PropertyList;
